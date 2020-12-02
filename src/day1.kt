0d60f38d598bf60f5f7be4a6f1329c88b4b12779
Lucas Overland <loverland@linkedin.com>
2020-12-02 10:24:36 -0600

    update to day 1

--- src/day1.kt
+++ src/day1.kt
@@ -1,45 +1,79 @@
 import java.io.File
 import java.lang.Integer.parseInt
 
+fun List<Int>.combination(size: Int): MutableList<List<Int>> {
+    val arr = mutableListOf<List<Int>>()
+
+    // Not sure how to make this repeat dynamically.
+    this.forEach { i ->
+        this.minus(0..i).forEach { j ->
+            if(size == 2) {
+                arr.add(arrayListOf(i, j))
+            } else {
+                this.minus(0..j).forEach { k ->
+                    arr.add(arrayListOf(i, j, k))
+                }
+            }
+        }
+    }
+    return arr
+}
+
+fun withCombination(size: Int, stream: File) {
+    val result = stream.readLines()
+        .map { parseInt(it) }
+        .combination(size)
+        .find { it.sum() == 2020 }
+        ?.reduce { acc, i ->  acc * i}
+
+    println(result)
+}
+
 
 fun main () {
     val stream = File("input_data/1")
-    part1(stream)
-    part2(stream)
-}
-
-fun part1(stream: File) {
-
-    val numArray = stream.readLines().map { parseInt(it) }
-
-    numArray.forEach { a ->
-
-        val result = numArray.find { b ->
-            b + a == 2020
-        }
-        if (result != null) {
-            println(a * result)
-            return
-        }
-    }
+    withCombination(2, stream)
+    withCombination(3, stream)
 }
 
 
-fun part2(stream: File) {
 
-    val numArray = stream.readLines().map { parseInt(it) }
+//
+// OLD ANSWERS
+//
 
-    numArray.forEach { a ->
-
-        numArray.forEach { b ->
-            val result = numArray.find { c ->
-                a + b + c == 2020
-            }
-            if (result != null) {
-                println(a * b * result)
-                return
-            }
-        }
-
-    }
-}
+//fun part1(stream: File) {
+//
+//    val numArray = stream.readLines().map { parseInt(it) }
+//
+//    numArray.forEach { a ->
+//
+//        val result = numArray.find { b ->
+//            b + a == 2020
+//        }
+//        if (result != null) {
+//            println(a * result)
+//            return
+//        }
+//    }
+//}
+//
+//
+//fun part2(stream: File) {
+//
+//    val numArray = stream.readLines().map { parseInt(it) }
+//
+//    numArray.forEach { a ->
+//
+//        numArray.forEach { b ->
+//            val result = numArray.find { c ->
+//                a + b + c == 2020
+//            }
+//            if (result != null) {
+//                println(a * b * result)
+//                return
+//            }
+//        }
+//
+//    }
+//}
