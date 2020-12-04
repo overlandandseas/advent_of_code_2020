64864d7939d785d0cde27c44849b17516dc8779f
Lucas Overland <loverland@linkedin.com>
2020-12-04 11:34:59 -0600

    Day 4

--- src/day3.kt
+++ src/day3.kt
@@ -19,16 +19,23 @@ private fun part1(stream: File) {
 
 private fun part2(stream: File) {
 
-    val result = stream.readLines().foldIndexed(arrayOf(0L, 0L, 0L, 0L, 0L)) { index, acc, s ->
+    val treeArr = arrayOf(0L, 0, 0, 0, 0)
 
-        if (s[index % s.length] == TREE) acc[0]++ // 0: right 1, down 1
-        if (s[index * 3 % s.length] == TREE) acc[1]++ // 1: right 3, down 1 (part 1)
-        if (s[index * 5 % s.length] == TREE) acc[2]++ // 2: right 5, down 1
-        if (s[index * 7 % s.length] == TREE) acc[3]++ // 3: right 7, down 1
-        if (index % 2 == 0 && s[(index / 2) % s.length] == TREE) acc[4]++ // 4: right 1, down 2
+    stream.readLines().forEachIndexed { index, s ->
+        if (s[index % s.length] == TREE) treeArr[0]++ // 0: right 1, down 1
+        if (s[index * 3 % s.length] == TREE) treeArr[1]++ // 1: right 3, down 1 (part 1)
+        if (s[index * 5 % s.length] == TREE) treeArr[2]++ // 2: right 5, down 1
+        if (s[index * 7 % s.length] == TREE) treeArr[3]++ // 3: right 7, down 1
+        if (index % 2 == 0 && s[(index / 2) % s.length] == TREE) treeArr[4]++ // 4: right 1, down 2
+    }
 
-        acc
-    }.reduce { acc, i -> acc * i }
+    println(treeArr.reduce { acc, l -> acc * l })
 
-    println(result)
+    treeArr.reduce(Long::times)
+    treeArr.reduce { acc, l -> acc * l }
+    treeArr.product()
+}
+
+private fun Array<Long>.product(): Long {
+    return this.reduce(Long::times)
 }
---
+++ src/day4.kt
@@ -0,0 +1,81 @@
+import java.io.File
+
+val VALID_FIELD_NAMES = arrayOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+
+val EYE_COLORS = arrayOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+
+fun main() {
+    val stream = File("input_data/4")
+    val passports = stream.readText().split("\n\n").map { it.split("\\s".toRegex()) }.map {
+
+        val passport = HashMap<String, String>()
+        it.forEach { record ->
+            if (record.contains(":")) {
+                val (key, value) = record.split(':')
+                passport[key] = value
+            }
+        }
+        passport
+    }
+    part1(passports)
+    part2(passports)
+}
+
+private fun part2(passports: List<HashMap<String, String>>) {
+    println(passports.count { isValidPassport(it) })
+}
+
+private fun part1(passports: List<HashMap<String, String>>) {
+    println(passports.count { hasRequiredFields(it) })
+}
+
+private fun isValidPassport(passport: Map<String, String>): Boolean {
+    return if (hasRequiredFields(passport)) {
+        passport.all {
+            when (it.key) {
+                "byr" -> checkYear(it.value, 1920, 2002)
+                "iyr" -> checkYear(it.value, 2010, 2020)
+                "eyr" -> checkYear(it.value, 2020, 2030)
+                "hgt" -> checkHeight(it.value)
+                "hcl" -> checkHairColor(it.value)
+                "ecl" -> checkEyeColor(it.value)
+                "pid" -> checkPassportID(it.value)
+                else -> true
+            }
+        }
+    } else false
+}
+
+private fun hasRequiredFields(passport: Map<String, String>): Boolean {
+    return VALID_FIELD_NAMES.all {
+        passport.containsKey(it)
+    }
+}
+
+private fun checkPassportID(str: String): Boolean {
+    return str.matches("[0-9]{9}".toRegex())
+}
+
+private fun checkHairColor(str: String): Boolean {
+    return str.matches("#[a-f0-9]{6}".toRegex())
+}
+
+private fun checkEyeColor(str: String): Boolean {
+    return EYE_COLORS.contains(str)
+}
+
+private fun checkYear(str: String, start: Int, end: Int): Boolean {
+    return str.toInt() in start..end
+}
+
+private fun checkHeight(str: String): Boolean {
+    return when {
+        str.contains("cm") -> {
+            str.split("cm")[0].toInt() in 150..193
+        }
+        str.contains("in") -> {
+            str.split("in")[0].toInt() in 59..76
+        }
+        else -> false
+    }
+}
