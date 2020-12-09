23f59af285dbd2d96c388d5a5c13ebcd8e2c049a
Lucas Overland <loverland@linkedin.com>
2020-12-09 16:58:26 -0600

    Day 9 part 2

--- src/day9.kt
+++ src/day9.kt
@@ -2,21 +2,6 @@ import java.io.File
 
 const val PREAMBLE_SIZE = 25
 
-fun main() {
-    val numbers = File("input_data/9").readLines().map { it.toLong() }
-
-    day1(numbers)
-}
-
-private fun day1(numbers: List<Long>) {
-    val amble = numbers.drop(PREAMBLE_SIZE)
-
-    val result = amble.findIndexed { index, i ->
-        !numbers.subList(index, PREAMBLE_SIZE + index).isSubsetSum(i)
-    }
-    println(result)
-}
-
 private fun List<Long>.isSubsetSum(i: Long): Boolean {
     return when {
         i == 0L -> true
@@ -26,11 +11,50 @@ private fun List<Long>.isSubsetSum(i: Long): Boolean {
     }
 }
 
-private fun <E> List<E>.findIndexed(function: (Int, E) -> Boolean): E? {
+private fun <E> List<E>.findIndexed(action: (index: Int, E) -> Boolean): E? {
     this.forEachIndexed { index, e ->
-        if (function(index, e)) {
+        if (action(index, e)) {
             return e
         }
     }
     return null
+}
+
+private fun List<Long>.getConnectedSubList(invalidNumber: Long): List<Long>? {
+    var l = 0
+    var sum = 0L
+    this.forEachIndexed { idx, value ->
+        while (sum > invalidNumber && l < idx - 1) {
+            sum -= this[l]
+            l++
+        }
+        if (sum == invalidNumber) {
+            return this.subList(l, idx)
+        }
+        sum += value
+    }
+    return null
+}
+
+private fun part1(numbers: List<Long>): Long? {
+    val amble = numbers.drop(PREAMBLE_SIZE)
+
+    return amble.findIndexed { index, i ->
+        !numbers.subList(index, PREAMBLE_SIZE + index).isSubsetSum(i)
+    }
+}
+
+private fun part2(numbers: List<Long>, invalidNumber: Long) {
+    val subList = numbers.getConnectedSubList(invalidNumber)
+    if (!subList.isNullOrEmpty()) {
+        println(subList.min()!! + subList.max()!!)
+    }
+}
+
+fun main() {
+    val numbers = File("input_data/9").readLines().map { it.toLong() }
+
+    val invalidNumber = part1(numbers)
+    println(invalidNumber)
+    part2(numbers, invalidNumber!!)
 }
